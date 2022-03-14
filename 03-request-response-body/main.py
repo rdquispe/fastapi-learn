044a5e848bdc15e595c9f9dfd6fb6859a85e8c40
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-13 21:56:30 -0500

    Validaciones: Query Parameters

--- 03-request-response-body/main.py
+++ 03-request-response-body/main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastApi
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -30,3 +30,13 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
+    age: Optional[str] = Query(...)
+):
+    return {name: age}
