625d2fbaa0324277dbc5871a4d078a21b2948d28
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-13 22:26:04 -0500

    Validaciones: Request Body

--- 03-request-response-body/main.py
+++ 03-request-response-body/main.py
@@ -20,6 +20,12 @@ class Person(BaseModel):
     is_married: Optional[bool] = None
 
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
 @app.get("/")
 def home():
     return {"Hello": "Rodrigo"}
@@ -36,8 +42,8 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-        name: Optional[str] = Query(default=None, min_length=1, max_length=50),
-        age: Optional[str] = Query(..., title="Person Age")
+    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
+    age: Optional[str] = Query(..., title="Person Age")
 ):
     return {name: age}
 
@@ -47,3 +53,16 @@ def show_person(
 @app.get("/person/detail/{person_id}")
 def show_person(person_id: int = Path(..., gt=0)):
     return {person_id: "exist"}
+
+
+# Validaciones: Request Body
+
+@app.put("/person/detail/{person_id}")
+def update_person(
+    person_id: int = Path(..., gt=0),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
