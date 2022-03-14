5a6d112d328e23b2531d9f74cb0dd489cd589c75
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-13 22:13:08 -0500

    Validaciones: Path Parameters

--- 03-request-response-body/main.py
+++ 03-request-response-body/main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastApi
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -36,7 +36,14 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
-    age: Optional[str] = Query(...)
+        name: Optional[str] = Query(default=None, min_length=1, max_length=50),
+        age: Optional[str] = Query(..., title="Person Age")
 ):
     return {name: age}
+
+
+# Validaciones: Path Parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(person_id: int = Path(..., gt=0)):
+    return {person_id: "exist"}
