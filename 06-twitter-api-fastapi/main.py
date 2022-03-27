dab58c1859e1f90f8c6e88622da8a8da21176d50
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-27 12:28:13 -0500

    Update main.py

--- 06-twitter-api-fastapi/main.py
+++ 06-twitter-api-fastapi/main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
@@ -25,7 +25,8 @@ class UserBase(BaseModel):
 class UserLogin(UserBase):
     password: str = Field(
         ...,
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 
@@ -44,7 +45,15 @@ class User(UserBase):
 
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 
 @app.get(path="/", status_code=status.HTTP_200_OK, tags=["Home"])
