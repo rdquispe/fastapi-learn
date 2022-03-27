98c2a858045e3463354918340b5b217cee8c3938
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-27 11:37:04 -0500

    Cookies and Headers Parameters

--- 05-module-project/README.md
+++ 05-module-project/README.md
@@ -4,3 +4,9 @@
 ```
 uvicorn main:app --reload  
 ```
+
+Install module EmailStr from Pydantic with Oh My Zsh
+
+```
+pipenv install "pydantic[email]"
+```
--- 05-module-project/main.py
+++ 05-module-project/main.py
@@ -3,10 +3,10 @@ from typing import Optional
 from enum import Enum
 
 # Pydantic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 # FastApi
-from fastapi import FastAPI, Body, Query, Path, status, Form
+from fastapi import FastAPI, Body, Query, Path, status, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -115,3 +115,28 @@ def update_person(
 @app.post(path="/login", response_model=LoginOut, status_code=status.HTTP_200_OK)
 def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
+
+
+# Cookies and Headers Parameters
+
+@app.post(path="/contact",status_code=status.HTTP_200_OK)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
