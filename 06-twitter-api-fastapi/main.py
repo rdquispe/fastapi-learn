1429bb5d1df8e553f0de8807b62dc9dfb14d7828
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-27 12:55:45 -0500

    Publish Tweets

--- 06-twitter-api-fastapi/main.py
+++ 06-twitter-api-fastapi/main.py
@@ -1,7 +1,8 @@
 # Python
 import json
 from uuid import UUID
-from datetime import date, datetime
+from datetime import date
+from datetime import datetime
 from typing import Optional, List
 
 # Pydantic
@@ -9,9 +10,10 @@ from pydantic import BaseModel
 from pydantic import EmailStr
 from pydantic import Field
 
-# FastApi
-from fastapi import HTTPException
-from fastapi import FastAPI, Body, Query, Path, status, Form, Header, Cookie, UploadFile, File
+# FastAPI
+from fastapi import FastAPI
+from fastapi import status
+from fastapi import Body
 
 app = FastAPI()
 
@@ -61,7 +63,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 
@@ -198,8 +200,34 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+    This path operation post a tweet in the app
+    Parameters:
+        - Request body parameter
+            - tweet: Tweet
+
+    Returns a json with the basic tweet information:
+        tweet_id: UUID
+        content: str
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 
 ### Show a tweet
